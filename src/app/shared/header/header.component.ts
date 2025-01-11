6cdac99e2ee85c806c4e7439bbf0ea332a7781d4
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-11 17:10:38 +0300

    Layout components created

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+}
