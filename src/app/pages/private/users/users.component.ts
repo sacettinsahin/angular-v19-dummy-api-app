08afca429c9b9ce1d15bedab4afef08e4ef9635a
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-12 19:50:27 +0300

    new features added to data table.

--- src/app/pages/private/users/users.component.ts
+++ src/app/pages/private/users/users.component.ts
@@ -7,16 +7,34 @@ import { PageHeaderComponent } from '../../../components/page-header/page-header
 import { BirthdatePipe } from '../../../pipes/birthdate.pipe';
 import { GenderPipe } from '../../../pipes/gender.pipe';
 import { PhonePipe } from '../../../pipes/phone.pipe';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+type Column = {
+  field:string;
+  header:string;
+  sortable:boolean;
+  selected:boolean;
+}
 
 @Component({
   selector: 'app-users',
   imports: [
     SpinnerComponent,
     TableModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    MultiSelectModule,
     PageHeaderComponent,
     BirthdatePipe,
     GenderPipe,
-    PhonePipe
+    PhonePipe,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
@@ -25,6 +43,9 @@ export class UsersComponent implements OnInit {
   users = signal<User[]>([]);
   usersService = inject(UsersService);
   isLoading = signal<boolean>(false);
+
+  selectedColumns:Column[] = [];
+  cols:Column[] = [];
 
   ngOnInit(): void {
     this.isLoading.set(true);
@@ -35,5 +56,28 @@ export class UsersComponent implements OnInit {
       (err) => console.log(err),
       () => this.isLoading.set(false)
     );
+    this.cols =[
+      {field:"id", header:"ID", sortable:true, selected:true},
+      {field:"firstName", header:"First Name", sortable:true, selected:true},
+      {field:"lastName", header:"Last Name", sortable:true, selected:true},
+      {field:"age", header:"Age", sortable:true, selected:true},
+      {field:"birthDate", header:"Date of Birth", sortable:false, selected:true},
+      {field:"phone", header:"Phone", sortable:false, selected:true},
+      {field:"role", header:"Role", sortable:true, selected:true},
+      {field:"email", header:"Email", sortable:false, selected:true},
+      {field:"gender", header:"Gender", sortable:true, selected:true},
+    ];
+
+    //select columns;
+    this.selectedColumns = this.cols;
+  }
+
+  change($event:any){
+    //console.log($event.itemValue)
+    $event.itemValue.selected = !$event.itemValue.selected;
+  }
+
+  showColumn(field:string): boolean {
+    return this.selectedColumns.some(col => col.field === `${field}`);
   }
 }
