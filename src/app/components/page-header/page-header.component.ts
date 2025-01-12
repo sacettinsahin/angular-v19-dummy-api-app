b3454bfbdec01665bd18b9adc80fdeb5eb10ce4b
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-12 11:14:11 +0300

    User component's data table created.

---
+++ src/app/components/page-header/page-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'page-header',
+  imports: [],
+  templateUrl: './page-header.component.html',
+  styleUrl: './page-header.component.scss'
+})
+export class PageHeaderComponent {
+  title = input.required<string>();
+}
--- src/app/pages/private/users/users.component.ts
+++ src/app/pages/private/users/users.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { User } from '../../../models/users.model';
 import { UsersService } from '../../../services/users.service';
 import { SpinnerComponent } from "../../../components/spinner/spinner.component";
-
+import { TableModule } from 'primeng/table';
+import { PageHeaderComponent } from "../../../components/page-header/page-header.component";
 
 @Component({
   selector: 'app-users',
-  imports: [SpinnerComponent],
+  imports: [SpinnerComponent, TableModule, PageHeaderComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
