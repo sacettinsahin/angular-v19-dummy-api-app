daf169b096949e60feb888f2779f6f782e1e08e1
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-12 09:19:38 +0300

    Components moved to private and public folders according to accesibility

--- src/app/pages/private/home/home.component.ts
+++ src/app/pages/private/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-home',
