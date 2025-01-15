f901fe91b4d0b7a0b357810fef87d2474ceb7e2e
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-15 08:54:10 +0300

    ngx-toastr added to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
@@ -6,5 +8,24 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow-sm': {
+          'text-shadow': '1px 1px 2px rgba(0, 0, 0, 0.1)',
+        },
+        '.text-shadow-md': {
+          'text-shadow': '2px 2px 4px rgba(0, 0, 0, 0.2)',
+        },
+        '.text-shadow-lg': {
+          'text-shadow': '4px 4px 6px rgba(0, 0, 0, 0.3)',
+        },
+        '.text-shadow-none': {
+          'text-shadow': 'none',
+        },
+      };
+
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
+};
