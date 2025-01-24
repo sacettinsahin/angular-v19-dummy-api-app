af238a3b89f0efae24222b13d5198bbbff1581e1
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2025-01-24 08:13:29 +0300

    Categories page header added

--- src/app/pages/private/categories/categories.component.ts
+++ src/app/pages/private/categories/categories.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { PageHeaderComponent } from "../../../components/page-header/page-header.component";
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [PageHeaderComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+
+  togglePopup(): void {}
 
 }
